14cc1a544ab17a8dac628a03c5bff8b7d5f4d207
Sharookhan B <sharookhan@users.noreply.github.com>
2020-07-25 23:00:41 +0530

    Active users error

--- static/js/app.js
+++ static/js/app.js
@@ -1,15 +1,6 @@
 $(document).ready(function()
 {
-    $.ajax({
-        type: "POST",
-        url: "/welcome",
-        cache: false,
-        context:this,     
-        success: function(data){
-            $('#print_all').html("<b>Active users :</b> "+data);
-          }
-    });
-
+    
     setInterval(function () {
     
        $.ajax({
